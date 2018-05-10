827acd2ece0c51aefe234194728b73c5424e0823
Chan Park <cwal1220@naver.com>
2018-05-10 22:00:50 +0900

    학식 DB

---
+++ sql/foodcourt.sql
@@ -0,0 +1,161 @@
+CREATE TABLE foodcourt
+(
+	index int,
+	name varchar(30),
+	restorant varchar(20),
+	price int
+)
+
+
+insert into foodcourt values(160, "철판참치김치덮밥", "김밥천국", 3500);
+insert into foodcourt values(158, "돌솥닭갈비", "김밥천국", 4000);
+insert into foodcourt values(500, "돌솥제육덮밥", "김밥천국", 4000);
+insert into foodcourt values(578, "닭갈비덮밥", "김밥천국", 3500);
+insert into foodcourt values(579, "철판쭈꾸미삼겹덮밥", "김밥천국", 4000);
+insert into foodcourt values(58, "치즈베이컨김치볶음밥", "김밥천국", 4000);
+insert into foodcourt values(63, "목살스테이크덮밥", "김밥천국", 4500);
+insert into foodcourt values(61, "치즈불고기볶음밥", "김밥천국", 4500);
+insert into foodcourt values(56, "돼지주물럭", "김밥천국", 4000);
+insert into foodcourt values(44, "매콤치킨커리", "김밥천국", 4000);
+insert into foodcourt values(134, "카레제육덮밥", "김밥천국", 3500);
+insert into foodcourt values(49, "참치비빔밥", "김밥천국", 3500);
+insert into foodcourt values(55, "물냉면", "김밥천국", 3500);
+insert into foodcourt values(54, "비빔냉면", "김밥천국", 3500);
+insert into foodcourt values(131, "냉쫄면", "김밥천국", 3500);
+insert into foodcourt values(157, "순두부찌개", "김밥천국", 3500);
+insert into foodcourt values(488, "참치김치만두찌개", "김밥천국", 4000);
+insert into foodcourt values(47, "야채김밥", "김밥천국", 1500);
+insert into foodcourt values(50, "참치김밥", "김밥천국", 2000);
+insert into foodcourt values(51, "라면", "김밥천국", 2000);
+insert into foodcourt values(64, "떡만두라면", "김밥천국", 3000);
+insert into foodcourt values(477, "모듬떡볶이", "김밥천국", 3500);
+insert into foodcourt values(53, "라볶이", "김밥천국", 2500);
+
+
+insert into foodcourt values(1, "등심돈까스", "U9", 4000);
+insert into foodcourt values(97, "U9우동+주먹밥", "U9", 3500);
+insert into foodcourt values(8, "갈릭양파돈까스", "U9", 4500);
+insert into foodcourt values(165, "콩나물불고기덮밥", "U9", 4000);
+insert into foodcourt values(13, "양푼이비빔밥", "U9", 3500);
+insert into foodcourt values(95, "부타동", "U9", 4000);
+insert into foodcourt values(4, "해물야끼우동", "U9", 4500);
+insert into foodcourt values(111, "우불덮밥", "U9", 4000);
+insert into foodcourt values(70, "달달불고기도시락", "U9", 4500);
+insert into foodcourt values(161, "커리미야자키치킨덮밥", "U9", 4500);
+insert into foodcourt values(15, "참치회덮밥", "U9", 3500);
+insert into foodcourt values(14, "냉모밀", "U9", 3500);
+insert into foodcourt values(108, "비빔모밀", "U9", 3500);
+insert into foodcourt values(5, "돈카츠벤또", "U9", 4000);
+insert into foodcourt values(85, "모둠컵밥", "U9", 3000);
+insert into foodcourt values(2, "소고기덮밥", "U9", 3000);
+insert into foodcourt values(99, "다꼬야끼치킨덮밥", "U9", 4000);
+insert into foodcourt values(6, "김치찌개돈까스", "U9", 4500);
+insert into foodcourt values(60, "파채돈까스", "U9", 4500);
+insert into foodcourt values(105, "고구마치즈돈까스", "U9", 4500);
+insert into foodcourt values(11, "치즈돈까스", "U9", 4000);
+insert into foodcourt values(12, "김치알돌솥밥", "U9", 4000);
+
+
+
+insert into foodcourt values(148, "양념감자오믈렛", "한스델리", 3500);
+insert into foodcourt values(16, "갈릭함박스테이크오믈렛", "한스델리", 00);
+insert into foodcourt values(26, "치킨볼오믈렛", "한스델리", 4000);
+insert into foodcourt values(24, "불닭오믈렛", "한스델리", 4000);
+insert into foodcourt values(82, "소고기필라프", "한스델리", 4000);
+insert into foodcourt values(22, "돈도리볶음밥", "한스델리", 3500);
+insert into foodcourt values(151, "소금구이덮밥", "한스델리",3500);
+insert into foodcourt values(23, "해물미트스파게티", "한스델리", 4500);
+insert into foodcourt values(28, "삼겹살강된장비빔밥", "한스델리", 4000);
+insert into foodcourt values(145, "매콤소금구이덮밥", "한스델리", 3500);
+insert into foodcourt values(19, "간장닭갈비잡채덮밥", "한스델리", 4000);
+insert into foodcourt values(153, "초계막국수", "한스델리", 3500);
+insert into foodcourt values(135, "초계비빔국수", "한스델리", 3500);
+insert into foodcourt values(20, "우삼겹숙주덮밥", "한스델리", 4000);
+insert into foodcourt values(18, "데리야끼치킨덮밥", "한스델리", 3500);
+insert into foodcourt values(177, "카오팟무", "한스델리", 3500);
+insert into foodcourt values(30, "고기국수", "한스델리", 4000);
+insert into foodcourt values(115, "제육장비빔밥", "한스델리", 3500);
+insert into foodcourt values(90, "매운참치비빔밥", "한스델리", 3500);
+insert into foodcourt values(21, "치즈오븐치킨라이스", "한스델리", 4000);
+insert into foodcourt values(123, "불닭치즈도리아", "한스델리", 4000);
+insert into foodcourt values(124, "모듬수제비", "한스델리", 4000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
